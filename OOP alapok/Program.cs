7e1e96405e88efd04bd23112a32690f8b0e3f7af
SzabolcsBodi756 <bodisz@kkszki.hu>
2025-09-05 12:11:23 +0200

    4-es is ready

--- OOP alapok/Program.cs
+++ OOP alapok/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,20 +10,40 @@ namespace OOP_alapok
 
     public class Szemely
     {
-        public string nev = "Kiss Péter";
+        public string Nev { get; set; }
+        public int Kor
+        {
+            get { return Kor; }
+            set 
+            {
+                if (Kor > 0)
+                {
+                    Kor = value;
+                }
+            }
+        }
 
-        public int kor = 35;
-        
+        public Szemely(string nev, int kor)
+        {
+            this.Nev = nev;
+            this.Kor = kor;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} jelenleg {Kor} éves";
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tanulo = new Szemely();
+            Szemely tanulo = new Szemely("Kiss Máté", 19);
 
-            Console.WriteLine(tanulo.nev);
-            Console.WriteLine(tanulo.kor);
+            
+            
+
         }
     }
 }
